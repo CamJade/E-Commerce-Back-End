be629ccb1235299791fc3caaab4521fa6266d4c0
Cameron White <whitcam20@gmail.com>
2021-10-27 01:04:41 -0400

    adding enpoints for tag model

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,83 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const findAllTags = await Tag.findAll ({
+      include: [
+        {model:Product}
+      ]
+    });
+    const allTags = findAllTags.map((tag) =>
+    tag.get({plain:true})
+    );
+    res.status(200).json(allTags);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagById = await Tag.findByPk(req.params.id, {
+      include: [
+        {model: Product}
+      ],
+    });
+    if (!tagById) {
+      res.status(404).json({ message: 'There is no tag found with this id.'});
+      return;
+    }
+    res.status(200).json(tagById);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    res.status(200).json(newTag);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    res.status(200).json(updateTag)
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Category.destroy({
+      where: {
+        id:req.params.id
+      }
+    });
+    if (!deleteTag) {
+      res.status(404).json({ message: 'No category was found with this tag!' });
+      return;
+    }
+    res.status(200).json(deleteTag);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
